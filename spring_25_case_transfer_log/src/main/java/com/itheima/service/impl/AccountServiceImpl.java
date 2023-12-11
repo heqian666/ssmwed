5c354c03abcb96edad170ebc1912e3d430cdf893
heqian666 <hq4290062001@.163.com>
2023-12-11 18:49:19 +0800

    更新

---
+++ spring_25_case_transfer_log/src/main/java/com/itheima/service/impl/AccountServiceImpl.java
@@ -0,0 +1,30 @@
+package com.itheima.service.impl;
+
+import com.itheima.dao.AccountDao;
+import com.itheima.service.AccountService;
+import com.itheima.service.LogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.*;
+
+@Service
+public class AccountServiceImpl implements AccountService {
+
+    @Autowired
+    private AccountDao accountDao;
+
+    @Autowired
+    private LogService logService;
+
+    public void transfer(String out,String in ,Double money) throws IOException{
+        try{
+            accountDao.outMoney(out,money);
+            accountDao.inMoney(in,money);
+        }finally {
+            logService.log(out,in,money);
+        }
+    }
+
+}
